f408e5b77b9b36c42400d4f798b20c5e5bdf5bae
Ammar <ammarahmed4517331@gmail.com>
2017-04-06 17:47:34 +0500

    practice

--- javascript/angular-prac.js
+++ javascript/angular-prac.js
@@ -1,9 +1,10 @@
 /**
  * Created by Ammar on 3/28/2017.
  */
+
 var myApp = angular.module("myApp", []);
 
-myApp.controller('mainController',['$scope','$filter', function($scope,$filter){
+myApp.controller('mainController',['$scope','$filter','$http', function($scope,$filter,$http){
 
     $scope.lowercasehandle=function(event){
 
@@ -14,6 +15,21 @@ myApp.controller('mainController',['$scope','$filter', function($scope,$filter){
         }
     };
 
+    $scope.rules=[
+        {rulename : 'Must be a 5 Characters'},
+        {rulename : 'Must be cool'},
+        {rulename : 'Must not be used elsewhere'}
+
+    ];
+
     $scope.characters = 5;
+
+    $http.get('/api')
+        .success(function(result){
+            $scope.rules = result;
+        })
+        .error(function(data , status){
+            console.log(data);
+        });
 
 }]);
